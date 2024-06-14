84f1cf0140f061854acda0e094ff16dcd381429d
Eduardo Bravo <eduardobravod@gmail.com>
2024-06-14 14:10:17 -0400

    First property render at html component, a few styles applied local and globally

--- angular-17-app/src/app/app.component.ts
+++ angular-17-app/src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'angular-17-app';
+  city = 'Santiago';
 }
